a39b5342a06cce0f61fb10d8c43f1f8ee5b96a9b
Joo Hyeon Jin <icepri3535@gmail.com>
2023-05-11 16:57:52 +0900

    29일차

---
+++ 230511/delete_elements_array.js
@@ -0,0 +1,15 @@
+// 정수 배열 arr과 delete_list가 있습니다. arr의 원소 중 delete_list의 원소를 모두 삭제하고 남은 원소들은 기존의 arr에 있던 순서를 유지한 배열을 return 하는 solution 함수를 작성해 주세요.
+
+function solution(arr, delete_list) {
+  let answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (delete_list.indexOf(arr[i]) === -1) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+solution([293, 1000, 395, 678, 94], [94, 777, 104, 1000, 1, 12]);
