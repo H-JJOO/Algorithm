af44ce8c3616b9fbf85523c9e91a20b97408a560
Joo Hyeon Jin <icepri3535@gmail.com>
2023-06-08 23:32:38 +0900

    57일차

--- 2306/230608/tie_string.js
+++ 2306/230608/tie_string.js
@@ -1,23 +1,25 @@
-function solution(strArr) {
-  let cnt = 0;
+// 문자열 배열 strArr이 주어집니다. strArr의 원소들을 길이가 같은 문자열들끼리 그룹으로 묶었을 때 가장 개수가 많은 그룹의 크기를 return 하는 solution 함수를 완성해 주세요.
 
-  let maxLength = 0;
+function solution(strArr) {
+  const groups = {};
 
   strArr.forEach((str) => {
-    if (str.length > maxLength) {
-      maxLength = str.length;
+    const len = str.length;
+    if (groups[len]) {
+      groups[len]++;
+    } else {
+      groups[len] = 1;
     }
   });
 
-  let countArray = new Array(maxLength).fill(0);
+  let maxGroupSize = 0;
+  for (const groupSize in groups) {
+    if (groups[groupSize] > maxGroupSize) {
+      maxGroupSize = groups[groupSize];
+    }
+  }
 
-  countArray.forEach((arr) => {
-    if ()
-  })
-
-  cnt = Math.max(...countArray);
-
-  return cnt;
+  return maxGroupSize;
 }
 
-solution(["a", "bc", "d", "efg", "hi"]);
+console.log(solution(["a", "bc", "d", "efg", "hi"]));
