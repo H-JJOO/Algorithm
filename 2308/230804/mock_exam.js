684e5f2649ed611e6ca75e7f050ecb63f9c26ed1
Joo Hyeon Jin <icepri3535@gmail.com>
2023-08-04 23:34:12 +0900

    114일차

---
+++ 2308/230804/mock_exam.js
@@ -0,0 +1,42 @@
+// 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+// 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+// 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+// 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+function solution(answers) {
+  let answer = [];
+  const arr = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  let cnt = [0, 0, 0];
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < answers.length; j++) {
+      if (answers[j] === arr[i][j % arr[i].length]) {
+        cnt[i]++;
+      }
+    }
+  }
+
+  let max = Math.max(cnt[0], cnt[1], cnt[2]);
+
+  if (max === cnt[0]) {
+    answer.push(1);
+  }
+  if (max === cnt[1]) {
+    answer.push(2);
+  }
+  if (max === cnt[2]) {
+    answer.push(3);
+  }
+
+  return answer;
+}
+
+console.log(solution([1, 3, 2, 4, 2]));
